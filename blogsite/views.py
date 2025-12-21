b03d0c244096dda75cfe9df220b870ca6292f520
SHAFI S <muhammedshafiknlr65@gmail.com>
2025-12-22 00:03:25 +0530

    Recent Articles showed in the Home page

--- blogsite/views.py
+++ blogsite/views.py
@@ -3,9 +3,11 @@ from blogs.models import Category, Blogs
 
 def home(request):
     categories = Category.objects.all()
-    featured_posts = Blogs.objects.filter(is_featured=True).order_by('-updated_at')
+    featured_posts = Blogs.objects.filter(is_featured=True, status='Published').order_by('-updated_at')
+    posts = Blogs.objects.filter(is_featured=False, status='Published')
     context = {
         'categories' : categories,
-        'featured_posts' : featured_posts
+        'featured_posts' : featured_posts,
+        'posts' : posts
     }
     return render(request, 'home.html', context)
