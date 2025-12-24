5618559100958607cc57258737edf04bafd62de2
SHAFI S <muhammedshafiknlr65@gmail.com>
2025-12-24 09:48:25 +0530

    Login & Logout functionality implemented

--- blogsite/urls.py
+++ blogsite/urls.py
@@ -28,6 +28,8 @@ urlpatterns = [
     path('category/', include('blogs.urls')),
     path('blogs/<slug:slug>', BlogsView.blogs, name='blogs'),
     path('blogs/search/', BlogsView.search, name='search'),
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
