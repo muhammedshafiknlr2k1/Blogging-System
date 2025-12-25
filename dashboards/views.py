4d2c74df38e6b4168509fc948ccbf05492dc066a
SHAFI S <muhammedshafiknlr65@gmail.com>
2025-12-25 18:27:27 +0530

    sideboards and dashboard categories implemented

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from blogs.models import Category, Blogs
 from django.contrib.auth.decorators import login_required
+from .forms import CategoryForm
+from django.shortcuts import get_object_or_404
 
 @login_required(login_url='login')
 def dashboard(request):
@@ -11,3 +13,41 @@ def dashboard(request):
         'blogs_count' : blogs_count
     }
     return render(request, 'dashboard/dashboard.html', context)
+
+
+def categories(request):
+    return render(request, 'dashboard/categories.html')
+
+def add_category(request):
+    if request.method=='POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
+    form = CategoryForm()
+    context = {
+        'form' : form
+    }
+    return render(request, 'dashboard/add_category.html', context)
+
+
+def edit_category(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
+    form = CategoryForm(instance=category)
+    context = {
+        'form' : form,
+        'category' : category
+    }
+    return render(request, 'dashboard/edit_category.html', context)
+
+
+def delete_category(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    category.delete()
+    return redirect('categories')
+    
