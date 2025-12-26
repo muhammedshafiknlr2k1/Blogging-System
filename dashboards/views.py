225e015acef2102159a57158d6ad0b91e15e9ed1
SHAFI S <muhammedshafiknlr65@gmail.com>
2025-12-26 12:34:37 +0530

    CRUD operations on users implemented

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import redirect, render
 from blogs.models import Category, Blogs
 from django.contrib.auth.decorators import login_required
-from .forms import BlogPostForm, CategoryForm
+from .forms import BlogPostForm, CategoryForm, AddUserForm, EditUserForm
 from django.shortcuts import get_object_or_404
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 
 @login_required(login_url='login')
@@ -67,7 +68,7 @@ def add_post(request):
     if request.method == 'POST':
         form = BlogPostForm(request.POST, request.FILES)
         if form.is_valid():
-            post = form.save(commit=False) #save form temporarily
+            post = form.save(commit=False)  # save form temporarily
             post.author = request.user
             post.save()
             title = form.cleaned_data['title']
@@ -96,8 +97,8 @@ def edit_post(request, pk):
             return redirect('posts')
     form = BlogPostForm(instance=post)
     context = {
-        'post' : post,
-        'form' : form
+        'post': post,
+        'form': form
     }
     return render(request, 'dashboard/edit_post.html', context)
 
@@ -106,3 +107,45 @@ def delete_post(request, pk):
     post = get_object_or_404(Blogs, pk=pk)
     post.delete()
     return redirect('posts')
+
+
+def users(request):
+    users = User.objects.all()
+    context = {
+        'users': users
+    }
+    return render(request, 'dashboard/users.html', context)
+
+
+def add_user(request):
+    if request.method == 'POST':
+        form = AddUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('users')
+    form = AddUserForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'dashboard/add_user.html', context)
+
+
+def edit_user(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    if request.method == 'POST':
+        form = EditUserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('users')
+    form = EditUserForm(instance=user)
+    context = {
+        'form': form,
+        'user': user
+    }
+    return render(request, 'dashboard/edit_user.html', context)
+
+
+def delete_user(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    user.delete()
+    return redirect('users')
